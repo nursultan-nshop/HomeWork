1baed3d6eebc483bea0bd41f8bf4301e280c04b3
Nursultan <abylajhanulyn@gmail.com>
2024-10-31 14:16:58 +0500

    first commit

---
+++ hw3-week9-JS/script.js
@@ -0,0 +1,15 @@
+let name = 'Nursultan';
+console.log(name);
+console.log('name type: ' + typeof name);
+
+
+var age = 12;
+var age = age.toString();
+console.log(age);
+console.log('age type: ' + typeof age);
+
+
+var isPupil = false;
+isPupil = !isPupil;
+console.log('Is he a pupil?:' +  isPupil )
+console.log('isPupil type: ' + typeof isPupil)
